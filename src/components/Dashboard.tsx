0b2f4b48ace409fc15c77aa2ec0b6dcbfabfb519
Abhishekh669 <nepaliabhishekh669@gmail.com>
2024-07-13 18:37:32 +0545

    1 commit

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,7 @@
 "use client"
-import { useGetUserData } from '@/utils/hooks/queryHooks/useGetUserData';
-import { useSession } from 'next-auth/react'
 import React from 'react'
-import Loader from './Loader';
 
 function Dashboard() {
- const session = useSession();
- const { data, error, isLoading } = useGetUserData(
-   session?.data?.user._id as string
- );
- if (isLoading) return <Loader />;
-  
-
   
   return (
     <div>
