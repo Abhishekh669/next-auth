bcc5a6be9640e853c81d00457051dad491161f47
Abhishekh669 <nepaliabhishekh669@gmail.com>
2024-07-28 13:27:14 +0545

    adding small stuff

--- src/components/users/accounts/BankDetails.tsx
+++ src/components/users/accounts/BankDetails.tsx
@@ -1,4 +1,6 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { bankdetailtype } from '@/types/bankdetail.types'
+import { Users } from 'lucide-react'
 import React from 'react'
 
 interface DataType{
@@ -10,8 +12,28 @@ interface DataType{
 function BankDetails({data, isLoading, error} : DataType) {
     console.log("this is the data in teh bankdetail apage",data)
   return (
-    <div>
-        <span className="text-white">hello world</span>
+    <div className='text-green-600 p-4 flex flex-col gap-y-4'>
+        <div>
+            Your Accounts : 
+        </div>
+       <div className='flex flex-col gap-y-4'>
+       {
+            data && data.data && data.data.slice().reverse().map((account : bankdetailtype) =>(
+                <Card x-chunk="dashboard-01-chunk-0" className='p-2 flex flex-col gap-y-3'>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-[20px] font-semibold">{account.bank_name}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div>
+                Branch : {account.bank_branch}
+            </div>
+          </CardContent>
+        </Card>
+            ))
+
+        }
+       </div>
+         
     </div>
   )
 }
